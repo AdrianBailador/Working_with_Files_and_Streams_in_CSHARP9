f72959f4fcdaca9d7d4b43613740118b96f77ed2
Adrián Bailador <adrianbailador@hotmail.com>
2022-06-01 00:23:36 +0100

    Copying a file

--- DataProcessor/FileProcessor.cs
+++ DataProcessor/FileProcessor.cs
@@ -33,10 +33,15 @@ namespace DataProcessor
             //if (!Directory.Exists(backupDirectoryPath))
             //{
                 WriteLine($"Attempting to create {backupDirectoryPath}");
-                Directory.CreateDirectory(backupDirectoryPath); 
+                Directory.CreateDirectory(backupDirectoryPath);
 
             //}
 
+            // Copy file to backup dir
+            string inputFileName = Path.GetFileName(InputFilePath);
+            string backupFilePath = Path.Combine(backupDirectoryPath, inputFileName);
+            WriteLine($"Copying {InputFilePath} to {backupFilePath}");
+            File.Copy(InputFilePath, backupFilePath, true);
         }
     }
 }
